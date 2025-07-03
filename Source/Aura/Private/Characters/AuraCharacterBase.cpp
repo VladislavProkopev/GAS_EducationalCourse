f5531e0a5f6df15c71c72a13fd52ef5c07fb4762
Vladislav Prokopev <vladislavprokopevcplusplus@gmail.com>
2025-07-04 02:57:25 +0500

    BaseCharacter created

---
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
